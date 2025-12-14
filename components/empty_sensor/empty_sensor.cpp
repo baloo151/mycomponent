569c166e57cb166c596307eb12168d6701c99c70
JLB <baloo151@yahoo.fr>
2025-12-14 10:06:46 +0100

    logging moved in dump_config

--- components/empty_sensor/empty_sensor.cpp
+++ components/empty_sensor/empty_sensor.cpp
@@ -7,10 +7,6 @@ namespace empty_sensor {
 static const char *TAG = "empty_sensor.sensor";
 
 void EmptySensor::setup() {
-    // Afficher l'intervalle de mise à jour dans les logs
-    delay(500);
-    ESP_LOGD(TAG, "Update interval: %d ms", this->get_update_interval());  
-    ESP_LOGE(TAG, "Update interval: %d ms", this->get_update_interval());  
 }
 
 void EmptySensor::loop() {
@@ -26,6 +22,7 @@ void EmptySensor::update() {
 
 void EmptySensor::dump_config() {
     ESP_LOGCONFIG(TAG, "Empty sensor");
+    ESP_LOGCONFIG(TAG, "Update interval: %d ms", this->get_update_interval());  
 }
 
 } //namespace empty_sensor

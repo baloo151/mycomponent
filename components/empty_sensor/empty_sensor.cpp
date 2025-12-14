e0623dad51dcb8be7fa23189622953a63e542053
JLB <baloo151@yahoo.fr>
2025-12-14 09:34:31 +0100

    counter added in empty_sensor

--- components/empty_sensor/empty_sensor.cpp
+++ components/empty_sensor/empty_sensor.cpp
@@ -7,15 +7,18 @@ namespace empty_sensor {
 static const char *TAG = "empty_sensor.sensor";
 
 void EmptySensor::setup() {
-  
+    // Afficher l'intervalle de mise à jour dans les logs
+    ESP_LOGCONFIG(TAG, "Update interval: %d ms", this->get_update_interval());  
 }
 
 void EmptySensor::loop() {
-  
+    // Incrémenter le compteur toutes les secondes
+    counter++;  
 }
 
 void EmptySensor::update() {
-
+    // Publier la valeur du compteur
+    this->publish_state(counter);  // Publie la valeur du compteur sur le capteur
 }
 
 void EmptySensor::dump_config() {
--- components/empty_sensor/empty_sensor.h
+++ components/empty_sensor/empty_sensor.h
@@ -7,10 +7,16 @@ namespace esphome {
 namespace empty_sensor {
 
 class EmptySensor : public sensor::Sensor, public PollingComponent {
+    public:
+        EmptySensor() : counter(0) {}  // Initialisation du compteur
+
     void setup() override;
     void loop() override;
     void update() override;
     void dump_config() override;
+
+    private:
+        int counter;  // Variable pour le compteur
 };
 
 } //namespace empty_sensor
